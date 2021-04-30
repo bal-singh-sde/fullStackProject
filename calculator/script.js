031b402041a28947d1480168c691fd892a8f0eb2
bal-singh-sde <balsinghlearn@gmail.com>
2021-04-30 10:47:39 -0700

    fixed code

--- calculator/script.js
+++ calculator/script.js
@@ -3,10 +3,11 @@ const API = "https://workout-planner-team2.herokuapp.com/";
 document.getElementById("userForm").addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
+  e.preventDefault();
+
   if (document.getElementById("desc")) {
     removeDestination(document.getElementById("desc"));
   }
-  e.preventDefault();
 
   const age = e.target.age.value;
   const height = Math.round(
@@ -14,7 +15,6 @@ function handleSubmit(e) {
       lengthConverterInches(e.target.heightInches.value)
   );
   const weight = weightConverter(e.target.weight.value);
-  console.log(height);
   const activity = e.target.activity.value;
   const gender = e.target.gender.value;
   const calculate = e.target.calculate.value;
@@ -22,7 +22,7 @@ function handleSubmit(e) {
   const waist = lengthConverterInches(e.target.waist.value);
   const hip = lengthConverterInches(e.target.hip.value);
 
-  resetFormValues(e.target);
+  document.getElementById("userForm").reset();
 
   if (calculate === "bmi") {
     var options = {
@@ -30,10 +30,8 @@ function handleSubmit(e) {
       url: "https://workout-planner-team2.herokuapp.com/bmi",
       params: {
         age: age,
-        // gender: gender,
         height: height,
         weight: weight,
-        // activity: activity,
       },
     };
 
@@ -43,21 +41,18 @@ function handleSubmit(e) {
       .catch((err) => console.log(err));
 
     function display(data) {
-      console.log(data.calories);
-
       const cardCont = document.getElementById("cardCont");
       const newCardCont = document.createElement("div");
       newCardCont.setAttribute("class", "col-md-3 calories-card m-2");
       newCardCont.setAttribute("id", "newCardCont");
-      newCardCont.setAttribute(
-        "style",
-        "max-width: 50rem; margin: auto; border: 3px solid rgb(51, 51, 51); width: 50%; margin-top: 30px; min-width: 20rem;"
-      );
       cardCont.appendChild(newCardCont);
 
       const newCard = document.createElement("div");
       newCard.setAttribute("class", "card text-center");
-      newCard.setAttribute("style", "width: 18rem;");
+      newCard.setAttribute(
+        "style",
+        "width: 50rem; box-shadow: 2px 2px 2px 2px gray;"
+      );
       newCardCont.appendChild(newCard);
 
       const cardTitle = document.createElement("h5");
@@ -67,10 +62,11 @@ function handleSubmit(e) {
 
       const desc = document.createElement("p");
       desc.setAttribute("class", "card-text");
+      desc.setAttribute("id", "desc");
       desc.innerText = `Healthy bmi range = ${data.data.results.healthy_bmi_range}`;
       newCard.appendChild(desc);
     }
-  } else if (calculate === "calculator") {
+  } else if (calculate === "calorie") {
     var options = {
       method: "GET",
       url: "https://workout-planner-team2.herokuapp.com/calculator",
@@ -85,25 +81,22 @@ function handleSubmit(e) {
 
     axios
       .request(options)
-      .then((data) => display(data.data.calories))
+      .then((data) => display0(data.data.calories))
       .catch((err) => console.log(err));
 
-    function display(data) {
-      console.log(data.calories);
-
+    function display0(data) {
       const cardCont = document.getElementById("cardCont");
       const newCardCont = document.createElement("div");
       newCardCont.setAttribute("class", "col-md-3 calories-card m-2");
       newCardCont.setAttribute("id", "newCardCont");
-      newCardCont.setAttribute(
-        "style",
-        "max-width: 50rem; margin: auto; border: 3px solid rgb(51, 51, 51); width: 50%; margin-top: 30px; min-width: 20rem;"
-      );
       cardCont.appendChild(newCardCont);
 
       const newCard = document.createElement("div");
       newCard.setAttribute("class", "card-body text-center");
-      newCard.setAttribute("style", "width: 18rem;");
+      newCard.setAttribute(
+        "style",
+        "width: 50rem; box-shadow: 2px 2px 2px 2px gray;"
+      );
       newCardCont.appendChild(newCard);
 
       const cardTitle = document.createElement("h5");
@@ -117,7 +110,7 @@ function handleSubmit(e) {
       desc.innerText = `calories you'll burn`;
       newCard.appendChild(desc);
     }
-  } else {
+  } else if (calculate === "advanced") {
     var options = {
       method: "GET",
       url: "https://workout-planner-team2.herokuapp.com/bodyFat",
@@ -135,36 +128,61 @@ function handleSubmit(e) {
 
     axios
       .request(options)
-      .then((data) => console.log(data.value))
+      .then((data) => display1(data))
       .catch((err) => console.log(err));
+
+    function display1(data) {
+      const cardCont = document.getElementById("cardCont");
+      const newCardCont = document.createElement("div");
+      newCardCont.setAttribute("class", "col-md-3 calories-card m-2");
+      newCardCont.setAttribute("id", "newCardCont");
+      cardCont.appendChild(newCardCont);
+
+      const newCard = document.createElement("div");
+      newCard.setAttribute("class", "card-body text-center");
+      newCard.setAttribute(
+        "style",
+        "width: 50rem; box-shadow: 2px 2px 2px 2px gray;"
+      );
+      newCardCont.appendChild(newCard);
+
+      const cardTitle = document.createElement("h5");
+      cardTitle.innerHTML = `Body Fat Mass = ${Math.round(
+        data.data.results["Body Fat Mass"]
+      )} -- Lean Body Mass = ${Math.round(
+        data.data.results["Lean Body Mass"]
+      )} -- Body Fat (BMI method) = ${Math.round(
+        data.data.results["Body Fat (BMI method)"]
+      )}`;
+      cardTitle.setAttribute("class", "card-title");
+      newCard.appendChild(cardTitle);
+
+      const desc = document.createElement("p");
+      desc.setAttribute("class", "card-text");
+      desc.setAttribute("id", "desc");
+      desc.innerText = `${data.data.results["Message"]}`;
+      newCard.appendChild(desc);
+    }
   }
-}
 
-function resetFormValues(form) {
-  // Go through all the form values and reset their values
-
-  for (var i = 0; i < form.length; i++) {
-    form.elements[i].value = "";
+  function removeDestination(event) {
+    var cardBody = event.parentElement;
+    var card = cardBody.parentElement;
+    card.remove();
   }
-}
 
-function removeDestination(event) {
-  var cardBody = event.parentElement;
-  var card = cardBody.parentElement;
-  card.remove();
-}
+  // lbs to kg
+  function weightConverter(valNum) {
+    return valNum / 2.2046;
+  }
 
-// lbs to kg
-function weightConverter(valNum) {
-  return valNum / 2.2046;
-}
+  //feet to cm
+  function lengthConverterFeet(valNum) {
+    return valNum / 0.032808;
+  }
 
-//feet to cm
-function lengthConverterFeet(valNum) {
-  return valNum / 0.032808;
-}
-
-// inches to cm
-function lengthConverterInches(valNum) {
-  return valNum / 0.3937;
+  // inches to cm
+  function lengthConverterInches(valNum) {
+    return valNum / 0.3937;
+  }
 }
